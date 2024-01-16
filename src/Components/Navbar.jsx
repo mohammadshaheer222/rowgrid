d0a81c67fa89e1cc78afbb8d4938840fa40a7dfa
mohammad shaheer <shaheercse2022@gmail.com>
2024-01-17 00:04:45 +0530

    Home page created

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -14,35 +14,46 @@ const Navbar = () => {
   return (
     <>
       <div className="space-y-16">
-        <nav className="flex items-center justify-between px-4 bg-primary fixed top-0 left-0 right-0 z-10 md:px-6 ">
-          <div className="flex items-center gap-2 ">
-          <img
-            className="w-10 h-10"
-            src="/src/assets/images/black rowgrid.png"
-            alt=""
-          />
-          <h1>Rowgrid</h1>
-          </div>
-          
-          <div className="flex items-center gap-5">
-            <p className="cursor-pointer text-sm tracking-wide font-semibold border-[0] border-b border-solid ">
-              LET'S TALK
-            </p>
+        <div>
+          <nav className="flex items-center justify-between px-4 bg-primary fixed top-0 left-0 right-0 z-20 md:px-6 ">
+            <div className="flex items-center gap-2 ">
+              <img
+                className="w-10 h-10"
+                src="/src/assets/images/black rowgrid.png"
+                alt=""
+              />
+              <h1>Rowgrid</h1>
+            </div>
 
-            <h1
-              onClick={openMenu}
-              className="cursor-pointer text-sm tracking-wide font-semibold border-[0] border-b border-solid"
-            >
-              {isOpen ? "CLOSE" : "MENU "}
-            </h1>
-          </div>
-        </nav>
+            <div className="flex items-center gap-5">
+              <p className="cursor-pointer text-sm tracking-wide font-semibold border-[0] border-b border-solid ">
+                LET'S TALK
+              </p>
+
+              <h1
+                onClick={openMenu}
+                className="cursor-pointer text-sm tracking-wide font-semibold border-[0] border-b border-solid"
+              >
+                {isOpen ? "CLOSE" : "MENU "}
+              </h1>
+            </div>
+          </nav>
+        </div>
+
         {isOpen && (
-          <div className="h-screen text-lg flex flex-col justify-center items-center gap-10 ">
-            <Link onClick={closeMenu} className="no-underline text-text">Home</Link>
-            <Link onClick={closeMenu} className="no-underline text-text">About</Link>
-            <Link onClick={closeMenu} className="no-underline text-text">Services</Link>
-            <Link onClick={closeMenu} className="no-underline text-text">Projects</Link>
+          <div className="h-screen bg-primary text-lg flex flex-col justify-center items-center gap-10 fixed top-0 left-0 right-0 z-10">
+            <Link onClick={closeMenu} className="no-underline text-text">
+              Home
+            </Link>
+            <Link onClick={closeMenu} className="no-underline text-text">
+              About
+            </Link>
+            <Link onClick={closeMenu} className="no-underline text-text">
+              Services
+            </Link>
+            <Link onClick={closeMenu} className="no-underline text-text">
+              Projects
+            </Link>
           </div>
         )}
         <Outlet />
